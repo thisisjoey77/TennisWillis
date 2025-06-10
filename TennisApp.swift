80d00416e6b39a18388c294f3a33052d059f4d3c
Joy Kim <94695542+thisisjoey77@users.noreply.github.com>
2025-06-10 21:46:41 +0900

    Update TennisApp.swift

--- TennisApp.swift
+++ TennisApp.swift
@@ -2,12 +2,20 @@ import SwiftUI
 
 @main
 struct TennisApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+
     init() {
-            UIView.appearance().overrideUserInterfaceStyle = .light
-        }
+        UIView.appearance().overrideUserInterfaceStyle = .light
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .onChange(of: scenePhase) { newPhase in
+            if newPhase == .background {
+                TeamDataManager.save(Teams)
+            }
         }
     }
 }
